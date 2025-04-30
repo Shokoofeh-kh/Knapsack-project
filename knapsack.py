82f2be3b56cfb150f9bd75592bbbb34df42d49d9
Shokoofeh <shokoofehkheirdastan@gmail.com>
2025-04-30 14:51:28 +0200

    Moving // Updating requirements

---
+++ knapsack.py
@@ -0,0 +1,82 @@
+import random
+
+from scipy.optimize import linprog
+import pandas as pd
+
+
+def generate_sample_dataset(total_num: int, num_items_low: int, num_items_high: int, save_dataset=False) -> pd.DataFrame:
+    assert num_items_low <= num_items_high
+    assert total_num >= num_items_high - num_items_low + 1, "higher total_num is required."
+    out = pd.DataFrame(
+        columns=[
+            'capacity', 'num_items', 'total_value', 'items_list (weight, value)',
+            'solution_value', 'solution_list (weight, value)'
+        ]
+    )
+    per_num_item_instances = total_num // (num_items_high - num_items_low)
+    print(per_num_item_instances)
+    for num_items in range(num_items_low, num_items_high):
+        for idx in range(per_num_item_instances):
+            instance = generate_instance(num_items)
+            solution_list, solution_value = instance.solve_lin_prog()
+
+            next_line = [
+                instance.capacity, num_items, instance.get_total_value(),
+                [(i.weight, i.value) for i in instance.available_items],
+                solution_value,
+                [(i.weight, i.value) for i in solution_list]
+            ]
+            out.loc[len(out)] = next_line
+
+    if save_dataset:
+        out.to_csv('knapsack_dataset.csv', index=True)
+    return out
+
+
+def generate_instance(num_items=10, max_weight=10, max_value=100, capacity_factor=0.5):
+    """
+    Generate a random knapsack instance with given constraints.
+    :param num_items: Number of items to generate.
+    :param max_weight: Maximum weight of each item.
+    :param max_value: Maximum value of each item.
+    :param capacity_factor: Factor to determine knapsack capacity based on total item weight.
+    :return: Knapsack instance.
+    """
+    items = [Item(random.randint(1, max_weight), random.randint(1, max_value)) for _ in range(num_items)]
+
+    total_weight = sum(item.weight for item in items)
+    capacity = int(total_weight * capacity_factor)
+
+    return Knapsack(capacity, items)
+
+
+class Item:
+    def __init__(self, weight: float | int, value: float | int):
+        self.weight = weight
+        self.value = value
+
+
+class Knapsack:
+    def __init__(self, capacity: float | int, available_items: list[Item]):
+        self.capacity = capacity
+        self.available_items = available_items
+
+    def get_total_value(self):
+        return sum([i.value for i in self.available_items])
+
+    def check_validity(self, item_set: list[Item]) -> bool:
+        return sum([i.weight for i in item_set]) < self.capacity
+
+    def solve_lin_prog(self) -> tuple[list[Item], float]:
+        num_items = len(self.available_items)
+        values = [-item.value for item in self.available_items]  # Maximize value (minimize negative value)
+        weights = [item.weight for item in self.available_items]
+
+        bounds = [(0, 1) for _ in range(num_items)]  # Binary constraint (item is taken or not)
+
+        result = linprog(c=values, A_ub=[weights], b_ub=[self.capacity], bounds=bounds, method='highs')
+
+        selected_items = [self.available_items[i] for i in range(num_items) if result.x[i] > 0.5]
+        total_value = sum(item.value for item in selected_items)
+
+        return selected_items, total_value
